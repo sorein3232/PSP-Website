ccb39436c0aedd0f782624868c89cdb6bc1456b7
sorein3232 <luiszara321@gmail.com>
2025-05-24 16:50:21 +0800

    new changes

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+    <script>
+        // Immediate redirect to frontend folder
+        window.location.href = '../frontend/index.html';
+    </script>
+    <meta http-equiv="refresh" content="0;url=./frontend/index.html">
+</head>
+<body>
+    <p>If you are not redirected automatically, <a href="./frontend/index.html">click here</a>.</p>
+</body>
+</html>
